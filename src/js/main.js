f5f27f655c8a6579219f210c13ab137a2b37669b
LeonardoGaldino <lcgm@cin.ufpe.br>
2017-11-26 14:48:00 -0300

    feat (Main.js and index.html): Handles multiple files
    Implements the necessary javascript and HTML to handle multiple files upload.

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
 var loadFile = () => {
 	var inputNode = document.getElementById('file-input-field'); //File Input DOM node
-	var inputFile = inputNode.files[0]; //File is always the first element in the array
-	var fileParser = new FileReader();
-	fileParser.readAsText(inputFile); //This is an async function
+	var inputFiles = inputNode.files; //Array of selected files
+	for(var idx in inputFiles) { //For each file, parse it and load its content
+		var file = inputFiles[idx];
+		var fileParser = new FileReader();
+		fileParser.readAsText(file); //This is an async function
 
-	fileParser.onload = function(loadEvent) { //Callback for file loaded
-		var parser = loadEvent.target;
-		var result = parser.result; //holds the file content
-		console.log(result);
-	};
+		fileParser.onload = function(loadEvent) { //Callback for file loaded
+			var parser = loadEvent.target;
+			var result = parser.result; //holds the file content
+			console.log(result);
+		};
+	}
 }
