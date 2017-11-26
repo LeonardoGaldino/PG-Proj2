e97958e355497f36a25388b597fe2cad58212a7a
LeonardoGaldino <lcgm@cin.ufpe.br>
2017-11-26 17:06:58 -0300

    feat (Application Exceptions): Creates custom exceptions
    Creates custom exceptions to handle invalid files.

---
+++ src/js/exceptions.js
@@ -0,0 +1,9 @@
+function FileReadException(message) {
+	this.message = message;
+	this.name = 'FileReadException';
+}
+
+function PointCoordinateParseException(message) {
+	this.message = message;
+	this.name = 'PointCoordinateParseException';
+}
